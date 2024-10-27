dfc6a92be4cefa1c9f9cc27893dd4ab2842944bb
Ramirez-05 <117038114+Ramirez-05@users.noreply.github.com>
2024-10-27 10:51:29 -0500

    refactor: Update HomeSpecialties component and styles

--- src/components/HomeSpecialties.vue
+++ src/components/HomeSpecialties.vue
@@ -17,7 +17,10 @@
                 :class="{ 'gastro-card': card.title === 'GASTRO' }" 
                 @click="handleCardClick(card.title)">
                 <ion-card-header>
-                    <div class="circular-container">
+                    <div 
+                        class="circular-container" 
+                        :style="{ borderColor: circleColors[index] }"
+                    >
                         <div class="circular-image-container">
                             <img :src="card.image" alt="imagen" class="card-image" />
                         </div>
@@ -30,6 +33,7 @@
         </div>
     </ion-content>
 </template>
+
 
 <script setup lang="ts">
 import { IonContent, IonCard, IonCardHeader, IonCardContent } from '@ionic/vue';
@@ -47,154 +51,171 @@ import medicinaImg from '@img/medicina.jpeg';
 const router = useRouter();
 
 const cards = [
-    { title: 'DOLOR', image: dolorImg},
-    { title: 'GASTRO', image: gastroImg},
-    { title: 'GINECO - UROLOGIA', image: urologiaImg},
-    { title: 'PEDIATRA - RESPIRATORIA', image: respirarImg},
-    { title: 'DERMATOLOGIA', image: dermaImg},
-    { title: 'HIDRISAGE', image: hidrisageImg},
-    { title: 'OFTAMOLOGIA', image: oftamologiaImg},
-    { title: 'MEDICINA GENERAL', image: medicinaImg},
+    { title: 'DOLOR', image: dolorImg },
+    { title: 'GASTRO', image: gastroImg },
+    { title: 'GINECO - UROLOGIA', image: urologiaImg },
+    { title: 'PEDIATRA - RESPIRATORIA', image: respirarImg },
+    { title: 'DERMATOLOGIA', image: dermaImg },
+    { title: 'HIDRISAGE', image: hidrisageImg },
+    { title: 'OFTAMOLOGIA', image: oftamologiaImg },
+    { title: 'MEDICINA GENERAL', image: medicinaImg },
+];
+
+const circleColors = [
+    'rgb(235, 57, 27)',
+    'rgb(16, 53, 136)',
+    'rgb(159, 250, 255)',
+    'rgb(8, 73, 132)',
+    'rgb(239, 192, 163)',
+    'rgb(146, 107, 0)',
+    'rgb(202, 159, 143)',
+    'rgb(193, 201, 214)',
 ];
 
 const handleCardClick = (title: string) => {
     if (title === 'GASTRO') {
-        //router.push('/ruta-a-la-que-deseas-redirigir');
         console.log('Gastro');
     }
 };
 </script>
 
+
 <style scoped>
+/* Estilos Globales */
 .content {
-    background-color: white; 
-    --ion-background-color: white; 
-    height: 100%; 
+  background-color: white; 
+  --ion-background-color: white; 
+  height: 100%; 
 }
 
 .outer-container {
-    background-color: white; 
-    padding: 16px; 
+  background-color: white; 
+  padding: 16px; 
 }
 
 .inner-container {
-    background-color: rgb(0, 130, 125); 
-    display: flex; 
-    flex-direction: column; 
-    align-items: center; 
-    justify-content: center; 
-    height: 100%; 
-    gap: 16px; 
-    position: relative; 
+  background-color: rgb(0, 130, 125); 
+  display: flex; 
+  flex-direction: column; 
+  align-items: center; 
+  justify-content: center; 
+  height: 100%; 
+  gap: 16px; 
+  position: relative; 
 }
 
 .container-2 {
-    padding: 10px; 
-    text-align: center; 
+  padding: 10px; 
+  text-align: center; 
 }
 
 .text-container {
-    display: flex;
-    flex-direction: column; 
-    align-items: center; 
+  display: flex;
+  flex-direction: column; 
+  align-items: center; 
 }
 
 .large-text {
-    font-size: 5vw; 
-    color: white; 
-    margin: 0; 
-    font-weight: bold;
+  font-size: 5vw; 
+  color: white; 
+  margin: 0; 
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3); 
 }
 
 .card-container {
-    display: flex;
-    flex-wrap: wrap; 
-    justify-content: center; 
-    gap: 10px;
+  display: flex;
+  flex-wrap: wrap; 
+  justify-content: center; 
+  gap: 10px;
 }
 
 .card {
-    width: calc(22% - 16px); 
-    margin-bottom: 16px; 
-    background-color: white;
-    box-shadow: none;
+  width: calc(22% - 16px); 
+  margin-bottom: 16px; 
+  background-color: white;
+  box-shadow: none;
 }
 
 .card-image {
-    width: 100%; 
-    height: auto; 
+  width: 100%; 
+  height: auto; 
 }
 
 .circular-container {
-    width: 160px; 
-    height: 160px; 
-    border-radius: 50%; 
-    border: 3px solid rgb(0, 130, 125); 
-    display: flex;
-    justify-content: center; 
-    align-items: center; 
-    margin: 0 auto; 
-    padding: 8px; 
-    box-sizing: border-box; 
+  width: 160px; 
+  height: 160px; 
+  border-radius: 50%; 
+  border: 3px solid rgb(0, 130, 125);
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
+  margin: 0 auto; 
+  padding: 8px; 
+  box-sizing: border-box; 
 }
 
 .circular-image-container {
-    width: 100%; 
-    height: 100%; 
-    border-radius: 50%; 
-    overflow: hidden; 
-    display: flex;
-    justify-content: center; 
-    align-items: center; 
+  width: 100%; 
+  height: 100%; 
+  border-radius: 50%; 
+  overflow: hidden; 
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
 }
 
 .circular-image-container img {
-    width: 100%; 
-    height: 100%; 
-    object-fit: cover; 
+  width: 100%; 
+  height: 100%; 
+  object-fit: cover; 
 }
 
 .card-content {
-    display: flex; 
-    justify-content: center; 
-    align-items: center; 
-    text-align: center; 
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  text-align: center; 
 }
 
 .card-title {
-    margin: 0; 
-    font-size: 1.2em; 
-    color: #00827d;
-    font-weight: bold; 
+  margin: 0; 
+  font-size: 1.2em; 
+  color: #00827d;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3); 
 }
 
+/* Media Queries */
 @media (min-width: 1025px) {
-    .gastro-card {
-        cursor: pointer; 
-    }
+  .gastro-card {
+    cursor: pointer; 
+  }
 }
 
 @media (max-width: 1024px) {
-    .card {
-        width: calc(25% - 16px);
-    }
+  .card {
+    width: calc(25% - 16px);
+  }
 }
 
 @media (max-width: 768px) {
-    .card {
-        width: calc(33% - 16px); 
-    }
+  .card {
+    width: calc(33% - 16px); 
+  }
+}
+
+@media (max-width: 580px) {
+  .card {
+    width: calc(50% - 16px); 
+  }
 }
 
 @media (max-width: 480px) {
-    .card {
-        width: calc(50% - 16px);
-    }
+  .card {
+    width: 100%; /* La tarjeta ocupará toda la fila en pantallas pequeñas */
+  }
 }
 
-@media (max-width: 320px) {
-    .card {
-        width: 100%; 
-    }
-}
 </style>
+
