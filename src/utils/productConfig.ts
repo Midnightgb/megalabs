c669b18a16ecfce1f5436110c8ec55905f35391c
Midnightgb <midnight3424@gmail.com>
2024-10-28 19:52:32 -0500

    hotfix #9

--- src/utils/productConfig.ts
+++ src/utils/productConfig.ts
@@ -1,23 +1,250 @@
+// src/utils/productConfig.ts
 import { Capacitor } from '@capacitor/core';
+/* dexgstrol: { line: 'gastro', range: [8, 15] },
+sansflu: { line: 'gastro', range: [16, 20], extra: [177] },
+pregesix: { line: 'gastro', range: [21, 28] },
+lunarium: { line: 'gastro', range: [29, 33], extra: [179] },
+combispas: { line: 'gastro', range: [34, 38], extra: [181] },
+kirruiz: { line: 'gastro', range: [39, 43], extra: [183] },
+'neocholal-s': { line: 'gastro', range: [44, 52], extra: [185] },
+enterex: { line: 'gastro', range: [53, 59] },
+lactipan: { line: 'gastro', range: [60, 64] },
+glutapak: { line: 'gastro', range: [65, 71] },
+phlebodia: { line: 'gastro', range: [72, 77] } */
+// Importar todas las imágenes dinámicamente
+// GASTRO - DEXGSTROL
+import dexgstrol8Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image8.webp';
+import dexgstrol8Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image8.jpg';
+import dexgstrol8TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image8.webp';
+import dexgstrol8TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image8.jpg';
+import dexgstrol9Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image9.webp';
+import dexgstrol9Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image9.jpg';
+import dexgstrol9TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image9.webp';
+import dexgstrol9TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image9.jpg';
+import dexgstrol10Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image10.webp';
+import dexgstrol10Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image10.jpg';
+import dexgstrol10TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image10.webp';
+import dexgstrol10TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image10.jpg';
+import dexgstrol11Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image11.webp';
+import dexgstrol11Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image11.jpg';
+import dexgstrol11TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image11.webp';
+import dexgstrol11TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image11.jpg';
+import dexgstrol12Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image12.webp';
+import dexgstrol12Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image12.jpg';
+import dexgstrol12TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image12.webp';
+import dexgstrol12TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image12.jpg';
+import dexgstrol13Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image13.webp';
+import dexgstrol13Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image13.jpg';
+import dexgstrol13TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image13.webp';
+import dexgstrol13TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image13.jpg';
+import dexgstrol14Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image14.webp';
+import dexgstrol14Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image14.jpg';
+import dexgstrol14TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image14.webp';
+import dexgstrol14TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image14.jpg';
+import dexgstrol15Webp from '@/assets/img/gastro/dexgstrol/desktop/webp/BOOK GAS 0924-2_page78_image15.webp';
+import dexgstrol15Jpg from '@/assets/img/gastro/dexgstrol/desktop/jpg/BOOK GAS 0924-2_page78_image15.jpg';
+import dexgstrol15TabletWebp from '@/assets/img/gastro/dexgstrol/tablet/webp/BOOK GAS 0924-2_page78_image15.webp';
+import dexgstrol15TabletJpg from '@/assets/img/gastro/dexgstrol/tablet/jpg/BOOK GAS 0924-2_page78_image15.jpg';
+
+// GASTRO - SANSFLU
+import sansflu16Webp from '@/assets/img/gastro/sansflu/desktop/webp/BOOK GAS 0924-2_page78_image16.webp';
+import sansflu16Jpg from '@/assets/img/gastro/sansflu/desktop/jpg/BOOK GAS 0924-2_page78_image16.jpg';
+import sansflu16TabletWebp from '@/assets/img/gastro/sansflu/tablet/webp/BOOK GAS 0924-2_page78_image16.webp';
+import sansflu16TabletJpg from '@/assets/img/gastro/sansflu/tablet/jpg/BOOK GAS 0924-2_page78_image16.jpg';
+import sansflu17Webp from '@/assets/img/gastro/sansflu/desktop/webp/BOOK GAS 0924-2_page78_image17.webp';
+import sansflu17Jpg from '@/assets/img/gastro/sansflu/desktop/jpg/BOOK GAS 0924-2_page78_image17.jpg';
+import sansflu17TabletWebp from '@/assets/img/gastro/sansflu/tablet/webp/BOOK GAS 0924-2_page78_image17.webp';
+import sansflu17TabletJpg from '@/assets/img/gastro/sansflu/tablet/jpg/BOOK GAS 0924-2_page78_image17.jpg';
+import sansflu18Webp from '@/assets/img/gastro/sansflu/desktop/webp/BOOK GAS 0924-2_page78_image18.webp';
+import sansflu18Jpg from '@/assets/img/gastro/sansflu/desktop/jpg/BOOK GAS 0924-2_page78_image18.jpg';
+import sansflu18TabletWebp from '@/assets/img/gastro/sansflu/tablet/webp/BOOK GAS 0924-2_page78_image18.webp';
+import sansflu18TabletJpg from '@/assets/img/gastro/sansflu/tablet/jpg/BOOK GAS 0924-2_page78_image18.jpg';
+import sansflu19Webp from '@/assets/img/gastro/sansflu/desktop/webp/BOOK GAS 0924-2_page78_image19.webp';
+import sansflu19Jpg from '@/assets/img/gastro/sansflu/desktop/jpg/BOOK GAS 0924-2_page78_image19.jpg';
+import sansflu19TabletWebp from '@/assets/img/gastro/sansflu/tablet/webp/BOOK GAS 0924-2_page78_image19.webp';
+import sansflu19TabletJpg from '@/assets/img/gastro/sansflu/tablet/jpg/BOOK GAS 0924-2_page78_image19.jpg';
+import sansflu20Webp from '@/assets/img/gastro/sansflu/desktop/webp/BOOK GAS 0924-2_page78_image20.webp';
+import sansflu20Jpg from '@/assets/img/gastro/sansflu/desktop/jpg/BOOK GAS 0924-2_page78_image20.jpg';
+import sansflu20TabletWebp from '@/assets/img/gastro/sansflu/tablet/webp/BOOK GAS 0924-2_page78_image20.webp';
+import sansflu20TabletJpg from '@/assets/img/gastro/sansflu/tablet/jpg/BOOK GAS 0924-2_page78_image20.jpg';
+import sansflu177Webp from '@/assets/img/gastro/sansflu/desktop/webp/BOOK GAS 0924-2_page78_image177.webp';
+import sansflu177Jpg from '@/assets/img/gastro/sansflu/desktop/jpg/BOOK GAS 0924-2_page78_image177.jpg';
+import sansflu177TabletWebp from '@/assets/img/gastro/sansflu/tablet/webp/BOOK GAS 0924-2_page78_image177.webp';
+import sansflu177TabletJpg from '@/assets/img/gastro/sansflu/tablet/jpg/BOOK GAS 0924-2_page78_image177.jpg';
 
 export interface ProductRange {
   line: ProductLine;
   range: readonly [number, number];
   extra?: readonly number[];
+  images: {
+    [key: number]: {
+      desktop: {
+        webp: string;
+        jpg: string;
+      };
+      tablet: {
+        webp: string;
+        jpg: string;
+      };
+    };
+  };
 }
 
 export const productRanges: Record<string, ProductRange> = {
-  dexgstrol: { line: 'gastro', range: [8, 15] },
-  sansflu: { line: 'gastro', range: [16, 20], extra: [177] },
-  pregesix: { line: 'gastro', range: [21, 28] },
-  lunarium: { line: 'gastro', range: [29, 33], extra: [179] },
-  combispas: { line: 'gastro', range: [34, 38], extra: [181] },
-  kirruiz: { line: 'gastro', range: [39, 43], extra: [183] },
-  'neocholal-s': { line: 'gastro', range: [44, 52], extra: [185] },
-  enterex: { line: 'gastro', range: [53, 59] },
-  lactipan: { line: 'gastro', range: [60, 64] },
-  glutapak: { line: 'gastro', range: [65, 71] },
-  phlebodia: { line: 'gastro', range: [72, 77] }
+  dexgstrol: {
+    line: 'gastro',
+    range: [8, 15],
+    images: {
+      8: {
+        desktop: {
+          webp: dexgstrol8Webp,
+          jpg: dexgstrol8Jpg
+        },
+        tablet: {
+          webp: dexgstrol8TabletWebp,
+          jpg: dexgstrol8TabletJpg
+        }
+      },
+      9: {
+        desktop: {
+          webp: dexgstrol9Webp,
+          jpg: dexgstrol9Jpg
+        },
+        tablet: {
+          webp: dexgstrol9TabletWebp,
+          jpg: dexgstrol9TabletJpg
+        }
+      },
+      10: {
+        desktop: {
+          webp: dexgstrol10Webp,
+          jpg: dexgstrol10Jpg
+        },
+        tablet: {
+          webp: dexgstrol10TabletWebp,
+          jpg: dexgstrol10TabletJpg
+        }
+      },
+      11: {
+        desktop: {
+          webp: dexgstrol11Webp,
+          jpg: dexgstrol11Jpg
+        },
+        tablet: {
+          webp: dexgstrol11TabletWebp,
+          jpg: dexgstrol11TabletJpg
+        }
+      },
+      12: {
+        desktop: {
+          webp: dexgstrol12Webp,
+          jpg: dexgstrol12Jpg
+        },
+        tablet: {
+          webp: dexgstrol12TabletWebp,
+          jpg: dexgstrol12TabletJpg
+        }
+      },
+      13: {
+        desktop: {
+          webp: dexgstrol13Webp,
+          jpg: dexgstrol13Jpg
+        },
+        tablet: {
+          webp: dexgstrol13TabletWebp,
+          jpg: dexgstrol13TabletJpg
+        }
+      },
+      14: {
+        desktop: {
+          webp: dexgstrol14Webp,
+          jpg: dexgstrol14Jpg
+        },
+        tablet: {
+          webp: dexgstrol14TabletWebp,
+          jpg: dexgstrol14TabletJpg
+        }
+      },
+      15: {
+        desktop: {
+          webp: dexgstrol15Webp,
+          jpg: dexgstrol15Jpg
+        },
+        tablet: {
+          webp: dexgstrol15TabletWebp,
+          jpg: dexgstrol15TabletJpg
+        }
+      },
+    }
+  },
+  sansflu: {
+    line: 'gastro',
+    range: [16, 20],
+    extra: [177],
+    images: {
+      16: {
+        desktop: {
+          webp: sansflu16Webp,
+          jpg: sansflu16Jpg
+        },
+        tablet: {
+          webp: sansflu16TabletWebp,
+          jpg: sansflu16TabletJpg
+        }
+      },
+      17: {
+        desktop: {
+          webp: sansflu17Webp,
+          jpg: sansflu17Jpg
+        },
+        tablet: {
+          webp: sansflu17TabletWebp,
+          jpg: sansflu17TabletJpg
+        }
+      },
+      18: {
+        desktop: {
+          webp: sansflu18Webp,
+          jpg: sansflu18Jpg
+        },
+        tablet: {
+          webp: sansflu18TabletWebp,
+          jpg: sansflu18TabletJpg
+        }
+      },
+      19: {
+        desktop: {
+          webp: sansflu19Webp,
+          jpg: sansflu19Jpg
+        },
+        tablet: {
+          webp: sansflu19TabletWebp,
+          jpg: sansflu19TabletJpg
+        }
+      },
+      20: {
+        desktop: {
+          webp: sansflu20Webp,
+          jpg: sansflu20Jpg
+        },
+        tablet: {
+          webp: sansflu20TabletWebp,
+          jpg: sansflu20TabletJpg
+        }
+      },
+      177: {
+        desktop: {
+          webp: sansflu177Webp,
+          jpg: sansflu177Jpg
+        },
+        tablet: {
+          webp: sansflu177TabletWebp,
+          jpg: sansflu177TabletJpg
+        }
+      },
+    }
+  },
+  // ... resto de productos
 } as const;
 
 export type ProductName = keyof typeof productRanges;
@@ -25,52 +252,28 @@ export type ProductLine = 'gastro' | 'dolor' | 'gineco - urologia' | 'pediatria 
 
 function getAssetPath(path: string): string {
   const isNative = Capacitor.isNativePlatform();
-  return isNative ? path.substring(1) : path;
-}
-
-function normalizeProductName(name: string): string {
-  return name.toLowerCase().trim();
-}
-
-function encodeImagePath(path: string): string {
-  const segments = path.split('/');
-  const fileName = segments.pop();
-  const encodedFileName = fileName ? encodeURIComponent(fileName) : '';
-  
-  // Normaliza todos los segmentos de la ruta a minúsculas
-  const normalizedSegments = segments.map(segment => segment.toLowerCase());
-  
-  return [...normalizedSegments, encodedFileName].join('/');
+  return isNative ? path : path;
 }
 
 export function getProductImages(product: ProductName, imageNumber: number) {
   const config = productRanges[product];
-  const normalizedProduct = normalizeProductName(product);
-  const normalizedLine = normalizeProductName(config.line);
-  const imageName = `BOOK GAS 0924-2_page78_image${imageNumber}`;
+  const images = config.images[imageNumber];
   
-  const baseUrl = `src/assets/img/${normalizedLine}/${normalizedProduct}`;
-  
-  // Crear las rutas
-  const paths = {
+  if (!images) {
+    console.error(`No images found for product ${product} number ${imageNumber}`);
+    return null;
+  }
+
+  return {
     desktop: {
-      webp: getAssetPath(encodeImagePath(`${baseUrl}/desktop/webp/${imageName}.webp`)),
-      jpg: getAssetPath(encodeImagePath(`${baseUrl}/desktop/jpg/${imageName}.jpg`))
+      webp: getAssetPath(images.desktop.webp),
+      jpg: getAssetPath(images.desktop.jpg)
     },
     tablet: {
-      webp: getAssetPath(encodeImagePath(`${baseUrl}/tablet/webp/${imageName}.webp`)),
-      jpg: getAssetPath(encodeImagePath(`${baseUrl}/tablet/jpg/${imageName}.jpg`))
+      webp: getAssetPath(images.tablet.webp),
+      jpg: getAssetPath(images.tablet.jpg)
     }
   };
-
-  // Log para verificar las rutas generadas
-  console.log(`[Image Paths] Product: ${normalizedProduct}, Number: ${imageNumber}`, {
-    desktop: paths.desktop,
-    tablet: paths.tablet,
-    isNative: Capacitor.isNativePlatform()
-  });
-
-  return paths;
 }
 
 export function getProductSlides(product: ProductName) {
@@ -80,51 +283,20 @@ export function getProductSlides(product: ProductName) {
   // Agregar imágenes del rango principal
   for (let i = config.range[0]; i <= config.range[1]; i++) {
     const images = getProductImages(product, i);
-    slides.push({
-      desktop: {
-        webp: images.desktop.webp,
-        jpg: images.desktop.jpg
-      },
-      tablet: {
-        webp: images.tablet.webp,
-        jpg: images.tablet.jpg
-      }
-    });
+    if (images) {
+      slides.push(images);
+    }
   }
   
   // Agregar imágenes extra si existen
   if (config.extra) {
     for (const pageNum of config.extra) {
       const images = getProductImages(product, pageNum);
-      slides.push({
-        desktop: {
-          webp: images.desktop.webp,
-          jpg: images.desktop.jpg
-        },
-        tablet: {
-          webp: images.tablet.webp,
-          jpg: images.tablet.jpg
-        }
-      });
+      if (images) {
+        slides.push(images);
+      }
     }
   }
   
   return slides;
-}
-
-// Función auxiliar para verificar rutas de archivos
-export function validateProductPaths(product: ProductName): void {
-  const config = productRanges[product];
-  const normalizedProduct = normalizeProductName(product);
-  
-  console.log(`[Validating Paths] Product: ${normalizedProduct}`);
-  
-  const slides = getProductSlides(product);
-  slides.forEach((slide, index) => {
-    Object.entries(slide).forEach(([device, formats]) => {
-      Object.entries(formats).forEach(([format, path]) => {
-        console.log(`[Path Check] ${device}/${format}: ${path}`);
-      });
-    });
-  });
 }
